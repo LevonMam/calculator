8d55bd6e71e40c70054b032e4121f5d892446bb9
Gor1194 <gorchshmarityan94@gmail.com>
2019-02-21 10:59:03 -0500

    Added difference

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 
 int main(){
-    int firstnum;
-    int secnum;
-    char gorc;
-cout<<"enter number 1"<<endl;
-cin >> firsnum>>endl;
-cout<<"enter number 2"<<endl;
-cin >> secnum>>endl;
-cout <<"gorcoxutyan nshan"<<endl;
-cin >> gorc;
+	int firstnum;
+	int secnum;
+	char gorc;
+	cout<<"enter number 1"<<endl;
+	cin >> firsnum>>endl;
+	cout<<"enter number 2"<<endl;
+	cin >> secnum>>endl;
+	cout <<"gorcoxutyan nshan"<<endl;
+	cin >> gorc;
+}
+
+
+
+int hamematum(int firstnum, int secnum)
+{
+
+	if(firstnum>secnum)
+	{
+		return firstnum;
+
+	}
+	else
+	{
+		return secnum;
+	}
+
+
+
 }
