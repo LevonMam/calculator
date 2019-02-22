212e15efbb750ee0a53eb9ef1d640e9dd4ddb047
Gor1194 <gorchshmarityan94@gmail.com>
2019-02-22 16:18:59 +0400

    Added bajanum

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,56 +5,56 @@ void calc();
 int main()
 {
 
-calc();
+	calc();
 
-return 0;
+	return 0;
 }
 
 void calc(){
 
-  int num1, num2;
-  char simvol;
-do{
-  cout<<"Enter num1 ";
-  cin>>num1;
-  cout<<"Enter num 2";
-  cin>>num2;
+	int num1, num2;
+	char simvol;
+	do{
+		cout<<"Enter num1 ";
+		cin>>num1;
+		cout<<"Enter num 2";
+		cin>>num2;
 
-  cout<<"Enter simvol ";
+		cout<<"Enter simvol ";
 
-  cin>> simvol;
-}while(!num1 || !num2 || simvol != '<' || simvol == '>' || simvol == '+' || simvol == '-' || simvol == '*' || simvol == '/');
+		cin>> simvol;
+	}while(!num1 || !num2 || simvol != '<' || simvol == '>' || simvol == '+' || simvol == '-' || simvol == '*' || simvol == '/');
 
- 
 
-    if(simvol == 'sum')
-    {
-	cout<<sum(num1,num2)<<endl;
-	return;
-    }
-    if(simvol =='<' || simvol == '>')
-    {
 
-	cout<<hamematum(num1,num2)<<endl;
-	return;
-    }
-    if(simvol == '-')
-    {
+	if(simvol == 'sum')
+	{
+		cout<<sum(num1,num2)<<endl;
+		return;
+	}
+	if(simvol =='<' || simvol == '>')
+	{
 
-	cout<<minus(num1,num2)<<endl;
-	return;
-    }
+		cout<<hamematum(num1,num2)<<endl;
+		return;
+	}
+	if(simvol == '-')
+	{
 
-    if(simvol == '/')
-    {
+		cout<<minus(num1,num2)<<endl;
+		return;
+	}
 
-	cout<<baj(num1,num2)<<endl;
-	return;
-    }
+	if(simvol == '/')
+	{
+
+		cout<<baj(num1,num2)<<endl;
+		return;
+	}
 
 }else{
-    cout<<"Enter normal num & simvol";
-    calc();
+	cout<<"Enter normal num & simvol";
+	calc();
 
 }
 int hamematum(int num1, int num2)
@@ -74,18 +74,37 @@ int hamematum(int num1, int num2)
 
 
 
-int minus(int num1, int num2)
-{   
-	int result;
+	int minus(int num1, int num2)
+	{   
+		int result;
 
-	result = num1 - num2;
-	return result;
+		result = num1 - num2;
+		return result;
 
 
-}
+	}
 
-int sum (int num1, int num2){
-int res;
-result=num1 + num2;
-return result;
-}
+	int sum (int num1, int num2){
+		int res;
+		result=num1 + num2;
+		return result;
+	}
+
+	int baj (int num1, int num2){
+		int result;
+
+		if(num2 == 0)
+		{
+			return 0;
+		}
+		else
+			result=num1/num2;
+		return result;
+
+	}
+
+
+
+
+
+
