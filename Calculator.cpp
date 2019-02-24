80179ad2f20aa9f3904aea691ac4c5e84ca25947
Gor1194 <gorchshmarityan94@gmail.com>
2019-02-24 19:45:46 +0400

    Calculator ready to usegit status!

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,94 +9,84 @@ int mnacord(int num1, int num2);
 int baj(int num1, int num2);
 int hamematum(int num1, int num2);
 int procent(int num1, int num2);
-void calc();
-
+double cubest (int num1,int num2);
+int bazm (int num1,int num2);
 int main()
 {
-
-	calc();
-
-	return 0;
-}
-
-void calc(){
-
 	int num1, num2;
-	char simvol;
-	while(simvol != num1 || simvol == '>' || simvol == '+' || simvol == '-' || simvol == '*' || simvol == '/'|| simvol == '$'|| simvol == '^'|| simvol == '@')
+	char symbol;
 
-	{
-		cout<<"Enter num1: ";
-		cin>>num1;
-		cout<<"Enter num2: ";
-		cin>>num2;
-
-		cout<<"Enter simvol: ";
-
-		cin>> simvol;
-
-		if(simvol == '+')
-		{
-			cout<<sum(num1,num2)<<endl;
-			return;
-		}
-		if(simvol =='<' || simvol == '>')
-		{
-
-			cout<<hamematum(num1,num2)<<endl;
-			return;
-		}
-		if(simvol == '-')
-		{
-
-			cout<<minuss(num1,num2)<<endl;
-			return;
-		}
-
-		if(simvol == '/')
-		{
-
-			cout<<baj(num1,num2)<<endl;
-			return;
-		}
-
-		if(simvol == '%')
-		{
-
-			cout<< mnacord(num1,num2)<<endl;
-			return;		
-		}
-
-		if(simvol == '^')
-		{
-
-			cout<< degree(num1,num2)<<endl;
-			return;
-		}
+	cout<<"Մուտ քա գրեք որևէ          symbol"<<endl;
+	cout<<"Գումա րում                   +"<<endl;
+	cout<<"Հա նում                      -"<<endl;
+	cout<<"Հա մեմա տ ում                <>"<<endl;
+	cout<<"Բա ժա նում                   /"<<endl;
+	cout<<"Բա զմա պ ա տ կում            *"<<endl;
+	cout<<"Մ նա ցորդ                    %"<<endl;
+	cout<<"Տոկոս                        $"<<endl;
+	cout<<"Թ վի ա ստ իճա ն              ^"<<endl;
+	cout<<"Թ վի ա րմա տ   խ որա նա րդ    @"<<endl<<endl;
 
 
-		if(simvol == '$')
-                {
 
-                        cout<< procent(num1,num2)<<endl;
-                        return;
-                }
-<<<<<<< HEAD
+	cout<<"Enter first number: ";
+	cin>>num1;
+	cout<<"Enter second: ";
+	cin>>num2;
+	cout<<"Enter symbol: ";
+	cin>> symbol;
 
-		 if(simvol == '@')
-                {
+	switch(symbol){
 
-                        cout<< cubest(num1,num2)<<endl;
-                        return;
-                }
+		case '+':
+			cout<<"RESULT:  "<<sum(num1,num2)<<endl;
+			break;
 
+		case '<':
+			cout<<"RESULT:  "<<hamematum(num1,num2)<<endl;
+			break;
+
+		case '-':
+			cout<<"RESULT:  "<<minuss(num1,num2)<<endl;
+			break;
+
+		case '/':
+			cout<<"RESULT:  "<<baj(num1,num2)<<endl;
+			break;
+
+		case '%':
+			cout<<"RESULT:  "<<mnacord(num1,num2)<<endl;
+			break;
+
+		case '^':
+			cout<<"RESULT:  "<<degree(num1,num2)<<endl;
+			break;
+
+		case '$':
+			cout<<"RESULT:  "<<procent(num1,num2)<<endl;
+			break;
+
+		case '@':
+			cout<<"RESULT:  "<<cubest(num1,num2)<<endl;
+			break;
+
+		case '*':
+			cout<<"RESULT:  "<<bazm(num1,num2)<<endl;
+			break;
 	}
+
+
 }
+
+
+
+
+
 int hamematum(int num1, int num2)
 {
 	if(num1>num2)
 	{
-	return num1;
+		return num1;
 
 	}
 	else
@@ -159,16 +149,23 @@ int mnacord(int num1, int num2){
 }
 
 double cubest (int num1,int num2){
-        cin.ignore(num2);
-        if (num1<0){
-        	double result =- pow(-num1, 1.0/3.0);
-	        return result;
-        } else if(num1>0) {
-        	double result = pow(num1, 1.0/3.0);
-      	  	return result;
-        } else {
-  	      return 0;
-        }
+	cin.ignore(num2);
+	if (num1<0){
+		double result =- pow(-num1, 1.0/3.0);
+		return result;
+	} else if(num1>0) {
+		double result = pow(num1, 1.0/3.0);
+		return result;
+	} else {
+		return 0;
+	}
 
- }
+}
+
+int bazm (int num1,int num2){
+	int result;
+	result=num1*num2;
+	return result;
+}
+
 
